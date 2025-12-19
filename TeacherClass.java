d1b8da115ad1f142b461624246063da2290ad9a0
Sidhant Saswat <sidhantsaswat19@gmail.com>
2025-12-19 16:20:12 +0530

    Refactor Teacher class constructor and methods

--- TeacherClass.java
+++ TeacherClass.java
@@ -1,30 +1,16 @@
-public class Teacher extends Employee{
-    String subject,qualification;
-    Teacher(String name,date dateOfBirth,date dateOfAppointment, String subject, String qualification){
-        this.name = name;
-        this.dob = dateOfBirth;
-        this.dateOfAppointment = dateOfAppointment;
-        this.subject = subject;
-        this.qualification = qualification;
-    }
-    @Override
-    void setSalary(int sal) {
-        this.salary = sal;
+class Teacher extends Employee {
+    String subject, qualification;
+
+    Teacher(String name, Date dob, Date doa, String subject, String qual) {
+        this.name = name; this.dob = dob; this.dateOfAppointment = doa;
+        this.subject = subject; this.qualification = qual;
     }
 
     @Override
-    int getSalary() {
-
-        return this.salary;
-    }
+    void setSalary(int sal) { super.updateSalary(sal); }
 
     @Override
     void getDetails() {
-        System.out.println("Name of Teacher: "+this.name);
-        System.out.println("Date of Birth: "+this.dob.getDate());
-        System.out.println("Date of Appointment: "+this.dateOfAppointment.getDate());
-        System.out.println("Subject: "+this.subject);
-        System.out.println("Qualifications: "+this.qualification);
-        System.out.println("Salary: "+this.getSalary());
+        System.out.println("\n[TEACHER] " + name + " | Subject: " + subject + " | Salary: $" + getSalary());
     }
 }
