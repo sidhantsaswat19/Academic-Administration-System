0bb554da5208e8116eebc4305d43703dde06a5d5
Sidhant Saswat <sidhantsaswat19@gmail.com>
2025-12-19 16:17:51 +0530

    Delete Dateclass.java

--- Dateclass.java
+++
@@ -1,7 +0,0 @@
-class Date implements Serializable {
-    int dd, mm, yy;
-    Date(int d, int m, int y){
-        this.dd = d; this.mm = m; this.yy = y;
-    }
-    String getDate(){ return dd+"-"+mm+"-"+yy; }
-}
