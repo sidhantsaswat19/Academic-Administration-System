10cf48039b90fde21be015845fc1b25bb9c1573a
Sidhant Saswat <sidhantsaswat19@gmail.com>
2025-12-19 16:21:10 +0530

    Refactor Student class and update getDetails method

--- StudentClass.java
+++ StudentClass.java
@@ -1,18 +1,13 @@
-public class Student extends Person{
+class Student extends Person {
     String subject;
-    Teacher teacher;
-    Student(String name, date date, Teacher teacher, String subject){
-        this.name = name;
-        this.dob = date;
-        this.teacher = teacher;
-        this.subject = subject;
+    Teacher teacher; // Association relationship
+
+    Student(String name, Date dob, Teacher teacher, String subject) {
+        this.name = name; this.dob = dob; this.teacher = teacher; this.subject = subject;
     }
 
     @Override
     void getDetails() {
-        System.out.println("Name of Student: "+this.name);
-        System.out.println("Date of Birth: "+this.dob.getDate());
-        System.out.println("Subject: "+this.subject);
-        System.out.println("Teacher: "+this.teacher.name);
+        System.out.println("\n[STUDENT] " + name + " | Course: " + subject + " | Mentor: " + teacher.name);
     }
 }
