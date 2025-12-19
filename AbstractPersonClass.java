adf1ddf1779fcce11909c2caa775cf55d68f5532
Sidhant Saswat <sidhantsaswat19@gmail.com>
2025-12-19 20:50:02 +0530

    Refactor comments in AbstractPersonClass
    Removed comment about abstract base class and updated formatting.

--- AbstractPersonClass.java
+++ AbstractPersonClass.java
@@ -1,9 +1,7 @@
-// 1. ABSTRACT BASE (Implements Serializable for File I/O)
 abstract class Person implements Serializable {
-    private static final long serialVersionUID = 1L; // Best practice for Serialization
+    private static final long serialVersionUID = 1L;
     String name;
     Date dob;
     
-    // Abstract method forces children to define their own display logic
     abstract void getDetails();
 }
