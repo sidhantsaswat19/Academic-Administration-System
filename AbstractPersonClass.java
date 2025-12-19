aebe2abcccd4051ea1052df5c296b5034b9a16c0
Sidhant Saswat <sidhantsaswat19@gmail.com>
2025-12-19 16:15:48 +0530

    Implement Serializable in AbstractPersonClass
    Updated Person class to implement Serializable and added serialVersionUID.

--- AbstractPersonClass.java
+++ AbstractPersonClass.java
@@ -1,5 +1,9 @@
-abstract class Person{
+// 1. ABSTRACT BASE (Implements Serializable for File I/O)
+abstract class Person implements Serializable {
+    private static final long serialVersionUID = 1L; // Best practice for Serialization
     String name;
-    date dob;
+    Date dob;
+    
+    // Abstract method forces children to define their own display logic
     abstract void getDetails();
 }
