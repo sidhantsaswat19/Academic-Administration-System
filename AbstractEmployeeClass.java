906b26b2824d046b254cb5bcb72770d4c20412f3
Sidhant Saswat <sidhantsaswat19@gmail.com>
2025-12-19 16:19:09 +0530

    Refactor Employee class for encapsulation and methods
    Updated Employee class to encapsulate salary and added a method to update it.

--- AbstractEmployeeClass.java
+++ AbstractEmployeeClass.java
@@ -1,6 +1,8 @@
-abstract class Employee extends Person{
-    date dateOfAppointment;
-    int salary;
+abstract class Employee extends Person {
+    Date dateOfAppointment;
+    private int salary; // Encapsulation: Private variable
+
     abstract void setSalary(int sal);
-    abstract int getSalary();
+    int getSalary() { return salary; }
+    void updateSalary(int sal) { this.salary = sal; }
 }
